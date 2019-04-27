7738486caa2219d6ef96e151aad0ccb9c1bab5f9
hasanfadool <hasasana68>
2019-04-28 01:23:08 +0300

    update some functions and add my problem/my update classes

---
+++ app/src/main/java/com/store/hasanfadool/mystore/ForNewVersion.java
@@ -0,0 +1,26 @@
+package com.store.hasanfadool.mystore;
+
+public class ForNewVersion {
+
+    // 1
+
+//    the user can rating the products
+
+    // 2
+
+//   add splash screen
+
+
+    // 3
+//    start getting the products from WS when start the splash screen
+
+    // 4
+
+//    the user can add notes or remarks for the products
+
+    // 5
+
+//    the user can add picture for his profile
+
+
+}
